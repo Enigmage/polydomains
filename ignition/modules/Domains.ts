bbd2c2a37eb3d0e00276c4590dc2c3536a1071af
I528580 <ali.abbas.zaidi@sap.com>
2024-11-14 21:38:59 +0530

    sequential execution

--- ignition/modules/Domains.ts
+++ ignition/modules/Domains.ts
@@ -6,8 +6,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const DomainsModule = buildModule("DomainsModule", m => {
   const domains = m.contract("Domains");
 
-  m.call(domains, "getAddress", ["doom"]);
-  m.call(domains, "register", ["doom"]);
+  const register = m.call(domains, "register", ["doom"]);
+  m.call(domains, "getAddress", ["doom"], { after: [register] });
 
   return { domains };
 });
