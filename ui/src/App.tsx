ed4e83844aa34a71eb21f8e5710cd825d78b0652
aviral49 <aviralt49@gmail.com>
2024-11-14 22:52:25 +0530

    add input form and way to interact with contract.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import './styles/App.css';
+import {ethers} from "ethers";
+
+import contractAbi from './utils/contractABI.json';
+
+const tld = '.ninja';
+const CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS_HERE';
 
 const App: React.FC = () => {
-
   const [currentAccount, setCurrentAccount] = useState('');
+  const [domain, setDomain] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [record, setRecord] = useState('');
 
-  const connectWallet = async () => {
+  const connectWallet = async () => { 
     try {
       const { ethereum } = window;
 
@@ -48,6 +56,53 @@ const App: React.FC = () => {
     }
   };
 
+  const mintDomain = async () => {
+    // Don't run if the domain is empty
+    if (!domain) { return }
+    // Alert the user if the domain is too short
+    if (domain.length < 3) {
+      alert('Domain must be at least 3 characters long');
+      return;
+    }
+    // Calculate price based on length of domain (change this to match your contract)	
+    // 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // const provider = new ethers.BrowserProvider(ethereum);
+        // const signer = await provider.getSigner();
+        // const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+  
+        // console.log("Going to pop wallet now to pay gas...")
+        // let tx = await contract.register(domain, {value: ethers.parseEther(price)});
+        // // Wait for the transaction to be mined
+        // const receipt = await tx.wait();
+  
+        // // Check if the transaction was successfully completed
+        // if (receipt.status === 1) {
+        //   console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+          
+        //   // Set the record for the domain
+        //   tx = await contract.setRecord(domain, record);
+        //   await tx.wait();
+  
+        //   console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+          
+        //   setRecord('');
+        //   setDomain('');
+        // }
+        // else {
+        //   alert("Transaction failed! Please try again");
+        // }
+      }
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
   // Create a function to render if wallet is not connected yet
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
@@ -57,6 +112,36 @@ const App: React.FC = () => {
       </button>
     </div>
   );
+
+  const renderInputForm = () =>{
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder='whats ur ninja power'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' disabled={undefined} onClick={mintDomain}>
+						Mint
+					</button>
+				</div>
+
+			</div>
+		);
+	} 
 
   // This runs our function when the page loads.
   useEffect(() => {
@@ -68,11 +153,15 @@ const App: React.FC = () => {
       <div className="container">
         <div className="header-container">
           <header>
-              <p className="title">PolyDomains</p>
-              <p className="subtitle">ENS on Polygon</p>
+              <div className="left">
+                <p className="title">PolyDomains</p>
+                <p className="subtitle">ENS on Polygon</p>
+              </div>
           </header>
         </div>
         {!currentAccount && renderNotConnectedContainer()}
+        {/* Render the input form if an account is connected */}
+				{currentAccount && renderInputForm()}
         <div className="footer-container">
         </div>
       </div>
