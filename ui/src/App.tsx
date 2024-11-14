4293bea7313bad6968ac113f25aa0c331f471646
aviral49 <aviralt49@gmail.com>
2024-11-14 21:38:40 +0530

    connect to metamask wallet

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 
 const App: React.FC = () => {
 
-  const checkIfWalletIsConnected = () => {
-    // First make sure we have access to window.ethereum
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    
+      // Boom! This should print out public address once we authorize Metamask.
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
 
     if (!ethereum) {
-      console.log("Make sure you have MetaMask!");
+      console.log('Make sure you have metamask!');
       return;
     } else {
-      console.log("We have the ethereum object", ethereum);
+      console.log('We have the ethereum object', ethereum);
     }
-  }
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // Users can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
 
   // Create a function to render if wallet is not connected yet
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
       <img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" /> <br/>
-      <button className="cta-button connect-wallet-button">
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
         Connect Wallet
       </button>
     </div>
@@ -39,7 +72,7 @@ const App: React.FC = () => {
               <p className="subtitle">ENS on Polygon</p>
           </header>
         </div>
-        {renderNotConnectedContainer()}
+        {!currentAccount && renderNotConnectedContainer()}
         <div className="footer-container">
         </div>
       </div>
