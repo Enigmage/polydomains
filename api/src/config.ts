0edf9ffdbcb7775f3d51297723dc8d4c6f8ab5dc
I528580 <ali.abbas.zaidi@sap.com>
2024-12-07 13:00:49 +0530

    add more routes; enhance contract

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import Domains from "../../contract/artifacts/contracts/Domains.sol/Domains.json";
 
 const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-const contractAddr = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const contractAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const walletPrivateKey =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
